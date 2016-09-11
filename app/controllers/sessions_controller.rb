27252ed9f70b66b23717f6f02a9352917b29e2ce
Liam Ellis <liamhellis@gmail.com>
2016-09-10 20:18:12 -0700

    dual log in feature implemented for both users and non-profits, pretty pumped about it

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,15 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
+    non_profit = NonProfit.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash.now.alert = "Email or Password is invalid"
+      flash.now.alert = "#{user.name} Logged In!"
       redirect_to user_path(user)
+    elsif non_profit && non_profit.authenticate(params[:password])
+      session[:non_profit_id] = non_profit.id
+      flash.now.alert = "#{non_profit.name} Logged In!"
+      redirect_to non_profit_path(non_profit)
     else
       flash.now.alert = "Email or Password is invalid"
       render :new
